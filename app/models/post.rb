02c4addc691cc0cdc1a0a0534168ac20819ddf2b
shane <willyums86@gmail.com>
2021-10-14 11:44:05 -0400

    New APP

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  attr_accessor :content, :name, :title
+ 
+  validates :name,  :presence => true
+  validates :title, :presence => true,
+                    :length => { :minimum => 5 }
+end
